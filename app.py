158d18ba83181b5abb55c16b41b7549d43530863
rushikesh-karande <135083635+rushikesh-karande@users.noreply.github.com>
2026-05-18 12:11:53 +0530

    Update home route message for CI/CD success

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello! App is running 🚀"
+    return "CI/CD Working Successfully"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
